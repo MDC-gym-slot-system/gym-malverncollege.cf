2615e8ed233a730728cda8c8cdf3a44dec4c67e8
unknown <benson@benchowchunman.com>
2022-10-13 12:11:34 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def sign_in():
+    return "This is the future sign in page"
+
+
+@app.route('/register')
+def register():
+    return "This is the future register page"
+
+
+@app.route('/reset_password')
+def reset_password():
+    return "This is the future reset password page"
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
