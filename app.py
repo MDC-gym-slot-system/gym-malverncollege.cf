dc79f192be1af77aa89951647aa3ea0d801d4617
unknown <benson@benchowchunman.com>
2022-10-14 16:45:05 +0100

    setting up flask routes

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def sign_in():
-    return "This is the future sign in page"
+    return render_template('sign_in.html')
 
 
 @app.route('/register')
 def register():
-    return "This is the future register page"
+    return render_template('register.html')
 
 
 @app.route('/password_reset')
 def reset_password():
-    return "This is the future reset password page"
+    return render_template('password_reset.html')
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
