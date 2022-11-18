7705479ed69900d70bd068afddf5c4db89a3627d
unknown <benson@benchowchunman.com>
2022-11-18 17:25:48 +0000

    added static email sheet example

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import pandas as pd
+
 app = Flask(__name__)
 
 
@@ -12,10 +14,11 @@ def sign_in():
 
     return render_template('sign_in.html')
 
-
-
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == "POST":
+        df = pd.read_excel(r'.\static\excel_sheets\emails.xlsx')
+        return df.to_html()
     return render_template('register.html')
 
 
