a26e9b029690f1dae5625efd040eaaac864be77c
unknown <benson@benchowchunman.com>
2022-11-27 01:06:17 +0000

    D

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-from flask import Flask, render_template, request, url_for, redirect, flash
-from pymongo import MongoClient
-from forms import LoginForm, RegisterForm, PasswordReset
 from os import getenv
+from forms import LoginForm, RegisterForm, PasswordReset
+
+from flask import Flask, render_template, request, url_for, redirect, flash
+from flask_mail import Mail, Message
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired, BadTimeSignature
+from pymongo import MongoClient
 from dotenv import load_dotenv
 load_dotenv()
-mongo_client = MongoClient(getenv("MongoDbSecretKey"))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = getenv("FlaskSecretKey")
+serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+
+mdc_challenge_cluster = MongoClient(getenv("MongoDbSecretKey"))
+website_db = mdc_challenge_cluster["website"]
+
 
 @app.route('/', methods=['GET', 'POST'])
 def sign_in():
@@ -18,16 +25,46 @@ def sign_in():
 
     return render_template('sign_in.html', form=form)
 
+def send_verification_email(email, token):
+    #need to set up email server, don't know how to do this
+    pass
+
+def save_user_in_database(email, password):
+    website_db["registered_accounts"].insert_one({"email": email, "password": password, "verified": False})
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
     if request.method == "POST":
         if form.validate_on_submit():
-            flash("You registered successfully", "success")
+            email, password = form.email.data, form.password.data
+            token = serializer.dumps(email, salt='email-confirm')
+
+            save_user_in_database(email, password)
+            send_verification_email(email, token)
+            flash(f"Your token is {token}", "success")  # for testing purposes
             return redirect(url_for('sign_in'))
 
-
     return render_template('register.html', form=form)
+
+
+def verify_user(email):
+    website_db["registered_accounts"].update_one({"email": email}, {"$set": {"verified": True}})
+
+
+@app.route('/verify_email/<token>', methods=['GET', 'POST'])
+def verify_email(token):
+    try:
+        email = serializer.loads(token, salt='email-confirm', max_age=3600)
+        verify_user(email)
+        flash("Your email has been verified", "success")
+        return redirect(url_for('sign_in'))
+    except SignatureExpired:
+        flash("Your token is expired, please register again", "danger")
+        return redirect(url_for('register'))
+    except BadTimeSignature:
+        flash("Your token is invalid, please register again", "danger")
+        return redirect(url_for('register'))
 
 
 @app.route('/password_reset', methods=['GET', 'POST'])
@@ -35,6 +72,7 @@ def password_reset():
     form = PasswordReset()
     if request.method == "POST":
         if form.validate_on_submit():
+
             flash("You reset your password successfully", "success")
             return redirect(url_for('sign_in'))
     return render_template('password_reset.html', form=PasswordReset())
--- forms.py
+++ forms.py
@@ -1,7 +1,17 @@
+from os import getenv
+from re import search
+
+from pandas import read_excel
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, ValidationError
 from wtforms.validators import DataRequired, Email, EqualTo
-from pandas import read_excel
+from markupsafe import Markup
+from pymongo import MongoClient
+from dotenv import load_dotenv
+load_dotenv()
+
+mdc_challenge_cluster = MongoClient(getenv("MongoDbSecretKey"))
+website_db = mdc_challenge_cluster["website"]
 
 
 class LoginForm(FlaskForm):
@@ -15,9 +25,31 @@ def is_email_allowed_to_register_account(form, email):
     if email.data not in emails_allowed_to_register_account['email'].values:
         raise ValidationError('Email not allowed to register an account')
 
+
 def is_email_already_registered(form, email):
-    #use database to check if email is already registered
-    pass
+    found_account = website_db["registered_accounts"].find_one({"email": email.data})
+    if found_account:
+        if not found_account["verified"]:
+            website_db["registered_accounts"].delete_many({"email": email.data})
+        else:
+            raise ValidationError('Email already registered, reset password if forgotten')
+
+
+def is_password_strong_enough(form, password):
+    password = password.data
+    length_error = len(password) < 8
+
+    digit_error = search(r"\d", password) is None
+
+    alphabet_error = search(r"[a-zA-Z]", password) is None
+
+    symbol_error = search(r"[ !#$%&'()*+,-./[\\\]^_`{|}~" + r'"]', password) is None
+
+    password_ok = not (length_error or digit_error or alphabet_error or symbol_error)
+    if not password_ok:
+        raise ValidationError(
+            Markup('Password must be at least 8 characters long.<br>Contain at least one digit, letter and symbol')
+        )
 
 
 class RegisterForm(FlaskForm):
@@ -30,10 +62,9 @@ class RegisterForm(FlaskForm):
             is_email_already_registered
         ]
     )
-    password = PasswordField('password', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired(), is_password_strong_enough])
     confirm_password = PasswordField('confirm_password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('register')
-
 
 
 class PasswordReset(FlaskForm):
